824d0801d714d2035f7d6b44b75746222aa7204d
Pratchaya Vongjongrak <pratchaya.vongjongrak@nanameue.jp>
2024-09-09 20:02:12 +0900

    feat: add mock chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { MockChat } from '@/components'
+
 export default function Index() {
   return (
     <div className='Index'>
-      <div className='Index__left'>left</div>
-      <div className='Index__right'>right</div>
+      <div className='Index__left'>
+        <MockChat />
+      </div>
+      <div className='Index__right'>TODO: form to custom here</div>
     </div>
   )
 }
